638012e7724e19f7fc662565ae11b6d3a0d89e49
Abod1296 <abodalrefa3i.1296@gmail.com>
2026-02-26 00:01:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from algoliasearch.search_client import SearchClient
 
 app = Flask(__name__)
@@ -9,22 +9,18 @@ ALGOLIA_APP_ID = os.environ.get("ALGOLIA_APP_ID")
 ALGOLIA_SEARCH_KEY = os.environ.get("ALGOLIA_SEARCH_KEY")
 ALGOLIA_INDEX = os.environ.get("ALGOLIA_INDEX")
 
-# Algolia client
 client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY)
 index = client.init_index(ALGOLIA_INDEX)
 
 @app.route("/")
 def home():
-    return "Algolia Cloud Run is working 🚀"
+    return "Algolia service is running 🚀"
 
 @app.route("/search")
 def search():
-    query = request.args.get("query", "")
-    results = index.search(query)
+    results = index.search("test")
     return jsonify(results)
 
 if name == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080))
-    )
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
