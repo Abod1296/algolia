9aa803b995b6a89c4975d29d5b83ce0a626215d6
Abod1296 <abodalrefa3i.1296@gmail.com>
2026-02-26 01:08:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import os
-from flask import Flask
-
-if name == "__main__":
-
 @app.route("/")
 def home():
-    return "Server is alive 🚀"
-
-
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+    print("15")
+    return "OK"
