269f7346b6510d41b206c8ad7d7edb045aa3229f
Abod1296 <abodalrefa3i.1296@gmail.com>
2026-02-26 01:24:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from algoliasearch.search_client import SearchClient
+import os
 
 app = Flask(__name__)
 
+# إعداد Algolia من Environment Variables
+ALGOLIA_APP_ID = os.environ.get("ALGOLIA_APP_ID")
+ALGOLIA_SEARCH_KEY = os.environ.get("ALGOLIA_SEARCH_KEY")
+ALGOLIA_INDEX = os.environ.get("ALGOLIA_INDEX")
+
+client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY)
+index = client.init_index(ALGOLIA_INDEX)
+
 @app.route("/")
 def home():
-    return "Oكس اختكK"
+    return "Algolia server is running 🚀"
+
+@app.route("/search")
+def search():
+    query = request.args.get("q", "")
+    results = index.search(query)
+    return jsonify(results)
